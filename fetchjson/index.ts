06ee4bb10220ad2a1c8f56d2866137d78405a7ec
omarabdelaziz404 <abdelazizo045@gmail.com>
2023-06-25 12:59:44 +0200

    typescrypt firstday(printing out tasks)

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,1 +1,27 @@
+import axios from "axios";
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logtodo(id, title, completed);
+   
+});
+
+const logtodo = (id: number ,title: string ,completed: boolean) => {
+    console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished: ${completed ? "finished" : "not finished"} 
+    `);
+};
 
